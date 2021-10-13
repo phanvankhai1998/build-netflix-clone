314d3182990fefc884fb747c1607ee50e9f93e0f
KhaiPhan <phan_van_khai123@yahoo.com>
2021-10-13 15:34:29 +0700

    Build the rows

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from './requests';
-
+import './Banner.css'
 
 function Banner() {
     
@@ -33,18 +33,37 @@ function Banner() {
 
     return (
         <div>
-            <header> {/* <<<< Backgroud image */}
+            <header className="banner"
+                style = {
+                    { 
+                        backgroudSize: "cover",
+                        backgroudImage: `url(
+                        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+                        )`,
+                        backgroundPosition: "center center",
+                    }
+                }
+            > {/* <<<< Backgroud image */}
+                <div className = "banner__contents">
+                    {/* title */}
+                    <h1>
+                        { movie?.title  || movie?.name || movie?.original_name}
+                    </h1>
+                    
+                    {/* div > 2 buttons */}
+                    <div className="banner__buttons">
+                        <button className="banner__button">Play</button>
+                        <button className="banner__button">My List</button>
+                    </div>
+                   
+                    <h1 className="banner_description">
+                        {movie?.overview}
+                    </h1>
 
-            {/* title */}
 
+                    {/* description */}
 
-
-            {/* div > 2 buttons */}
-
-
-
-            {/* description */}
-
+                </div>
             </header>
         </div>
     )
